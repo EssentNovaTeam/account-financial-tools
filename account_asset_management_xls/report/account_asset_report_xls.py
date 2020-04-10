35b1bc2ac55c1f6f157c96a9d6b05fc04ecb306c
Robin Conjour <robin@conjour.nl>
2020-04-10 08:01:12 +0200

    [FIX] Remove column perdiod depreciation total

--- account_asset_management_xls/report/account_asset_report_xls.py
+++ account_asset_management_xls/report/account_asset_report_xls.py
@@ -319,19 +319,6 @@ class AssetReportXls(report_xls):
                 'totals': [
                     1, 0, 'number', None, _render("total_depr_formula"),
                     self.rt_cell_style_decimal]},
-            'period_end_depr': {
-                'header': [
-                    1, 18, 'text', _render("_('Tot. Period Depreciation')"),
-                    None, self.rh_cell_style_right],
-                'asset_view': [
-                    1, 0, 'number', None, _render("period_depr_formula"),
-                    self.av_cell_style_decimal],
-                'asset': [
-                    1, 0, 'number', None, _render("period_depr_formula"),
-                    self.an_cell_style_decimal],
-                'totals': [
-                    1, 0, 'number', None, _render("period_depr_formula"),
-                    self.rt_cell_style_decimal]},
             'method': {
                 'header': [
                     1, 20, 'text', _render("_('Comput. Method')"),
@@ -887,8 +874,6 @@ class AssetReportXls(report_xls):
             period_diff_formula = (
                         period_start_value_cell + '-' +  # noqa: disable F841, report_xls namespace trick
                         period_end_value_cell)
-            period_depr_formula = (
-                    depreciation_base_cell + '-' + period_end_value_cell)  # noqa: disable F841, report_xls namespace trick
             # nova end
             total_depr_formula = depreciation_base_cell \
                 + '-' + fy_end_value_cell
@@ -969,8 +954,6 @@ class AssetReportXls(report_xls):
                                                   period_end_value_pos)
         period_end_value_cell = rowcol_to_cell(row_pos,  # noqa: disable F841, report_xls namespace trick
                                                period_end_value_pos)
-        period_depr_formula = (depreciation_base_cell + '-' +  # noqa: disable F841, report_xls namespace trick
-                               period_end_value_cell)
         # nova end
         fy_start_value_cell = rowcol_to_cell(row_pos, fy_start_value_pos)
         fy_end_value_cell = rowcol_to_cell(row_pos, fy_end_value_pos)
